31469d6acfab634489f5d9b8b76d4b94b508fa93
Roberto Gutierrez <rob7th@gmail.com>
2024-04-23 14:26:27 -0600

    update mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+let ham = document.getElementById("ham");
+
+function Open() {
+  hamBtn.classList.add("hidden");
+  ham.classList.remove("hidden");
+  closeBtn.classList.remove("hidden");
+}
+
+function Close() {
+  hamBtn.classList.remove("hidden");
+  ham.classList.add("hidden");
+  closeBtn.classList.add("hidden");
+}
