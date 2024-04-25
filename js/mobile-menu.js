c191578987d139b2a271af8cd66a030d9e8fb67a
Roberto Gutierrez <rob7th@gmail.com>
2024-04-25 15:07:08 -0600

    webbyGravy logo update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@ let ham = document.getElementById("ham");
 
 function Open() {
   hamBtn.classList.add("hidden");
+  ham.classList.add("duration-500");
   ham.classList.remove("hidden");
   closeBtn.classList.remove("hidden");
 }
